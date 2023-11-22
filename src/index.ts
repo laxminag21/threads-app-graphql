2a8f42eb06ccfb9a5955e612458975e13622b6ba
laxminag21 <laxminag2109@gmail.com>
2023-11-22 12:53:00 +0530

    third commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
+import { prismaClient } from './lib/db';
 
 async function init(){
     const app = express();
@@ -15,12 +16,21 @@ async function init(){
                 hello: String
                 say(name: String): String
             }
+            type Mutation{
+                createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+            }
         `,
         resolvers: {
             Query: {
                 hello: () => `Hey there, I am graphql server`,
                 say: (_, { name }: { name: string }) => `Hey ${name} Bro. How are you?`,
-            }
+            },
+            Mutation: {
+                createUser: async(_, 
+                    { firstName, lastName, email, password }: 
+                    { firstName: string; lastName: string; email: string; password: string }
+                    ) => {},
+            },
         },
     })
 
